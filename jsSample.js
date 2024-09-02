943f31996479747065b275027a0a6c18fbb68255
Matt Emrick <matt.emrick@gmail.com>
2024-09-02 10:43:38 -0500

    tutorial changes

---
+++ jsSample.js
@@ -0,0 +1,4 @@
+console.log ("--------");
+console.log ("rise and shine");
+console.log ("ready for a new day");
+console.log ("---------");
---
+++ pythonsample.py
@@ -0,0 +1,4 @@
+print ("----------------")
+print ("Python has entered the bulding")
+print ("Let the party start!")
+print ("-----------------")
